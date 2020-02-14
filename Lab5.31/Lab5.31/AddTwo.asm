0aa3abe86fe3379371bce8bc0167a82908fd2a57
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2020-02-14 10:05:16 -0500

    lab5

---
+++ Lab5.31/Lab5.31/AddTwo.asm
@@ -0,0 +1,20 @@
+; AddTwo.asm - adds two 32-bit integers.
+; Chapter 3 example
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+
+
+.code
+main PROC
+add al, 20
+add ax, 30
+mov eax, 1
+mov ebx, eax
+	invoke ExitProcess,0
+main endp
+end main
